2b5f6a1f0d6c1cd1c7898c11158d335e2b3cd1f0
David Hamaoui <davidhamaoui27@hotmail.com>
2023-02-04 03:40:02 -0500

    Links added, open new tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <!-- home page -->
 
 <script>
-
+    let url = "https://github.com/davidham96/logDavid/";
+    let interests = " data science, machine learning, functional programming";
 </script>
 
 <div class="index">
     <p>Resume + info about me are in <a href="/about">about</a>.</p>
     <p>Personal projects are in <a href="/projects">projects</a>.</p> <br>
-    <p>Current interests include data science, machine learning, functional programming.</p>
-    <p>The code for this site is available on <a href="" target="_blank">Github</a>.</p>
+    <p>Current interests include {interests}.</p>
+    <p>The code for this site is available on <a href="{url}" target="_blank" rel="noreferrer">Github</a>.</p>
 </div>
 
 <style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,13 +1,14 @@
 <!-- projects page -->
 
 <script>
-
+    let spaceInvaders = "https://github.com/davidham96/SpaceInvaders";
+    let logDavid = "https://github.com/davidham96/logDavid";
 </script>
 
 <div class="projects">
-    <h1>Space Invaders</h1>
+    <h1><a href="{spaceInvaders}" target="_blank" rel="noreferrer">Space Invaders</a></h1>
     <p>info about project, show .py files</p> <br><br>
-    <h1>log(David)</h1>
+    <h1><a href="{logDavid}" target="_blank" rel="noreferrer">log(David)</a></h1>
 </div>
 
 <style>
