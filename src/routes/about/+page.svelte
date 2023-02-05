8c3fb6b5c029e778983eb689b3293a974bec5cee
David Hamaoui <davidhamaoui27@hotmail.com>
2023-02-04 19:49:08 -0500

    Added goto feature

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,7 +8,6 @@
     <a href="DavidHamaouiResume.pdf" target="_blank">Resume</a>
     <p>U3 McGill Computer Science student (previously Physiology student, 2020-2022)
          interested in programming, Weightlifting, playing music.</p>
-    <p>add more detail here</p>
     <!-- svelte-ignore a11y-missing-attribute -->
     <iframe src="DavidHamaouiResume.pdf" width="60%" height="735px"></iframe>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,31 +3,36 @@
 <script>
     let spaceInvaders = "https://github.com/davidham96/SpaceInvaders";
     let logDavid = "https://github.com/davidham96/logDavid";
-    let spaceMain = "https://gist.github.com/davidham96/ffe6096aebf8d12e891a61d83124fe1a.js";
+    let spaceMain = "https://gist.github.com/davidham96/ffe6096aebf8d12e891a61d83124fe1a";
 </script>
 
 <div class="projects">
-    <!-- Space Invaders -->
-    <h2><a href="{spaceInvaders}" target="_blank" rel="noreferrer">Space Invaders</a></h2>
-    <p>Built Space Invaders in Python using pygame.</p>
-    <ol>Learned: <br><br>
-        <li>Working with game logic</li>
-        <li>Classes, inheritance, polymorphism</li>
-        <li>Managing files depending on classes/subclasses</li>
-    </ol>
-    <br>
-    <p>here show code and talk about it</p>
-    <script src="{spaceMain}"></script>
-    <br><br>
+
+    <p>goto <a href="#logDavid">log(David)</a></p><br>
+    <p>goto <a href="#spaceInvaders">Space Invaders</a></p><br>
+
     <!-- log(David) -->
-    <h2><a href="{logDavid}" target="_blank" rel="noreferrer">log(David)</a></h2>
-    <p>Built my first website using online tutorials and SvelteKit.</p>
-    <ol>Learned: <br><br>
-        <li>Basic HTML, CSS, Javascript, Svelte</li>
-        <li>Basic tools to create front-end design</li>
-    </ol>
-    <br>
-    <p>here show code and talk about it</p>
+    <section id="logDavid">
+        <h2><a href="{logDavid}" target="_blank" rel="noreferrer">log(David)</a> : Feb 2023</h2>
+        <p>Built my first website using online tutorials and SvelteKit.</p>
+        <ol>Learned: <br><br>
+            <li>Basic HTML, CSS, Javascript, Svelte</li>
+            <li>Basic tools to create front-end design</li>
+        </ol>
+    </section>
+    <br><br>
+
+    <!-- Space Invaders -->
+    <section id="spaceInvaders">
+        <h2><a href="{spaceInvaders}" target="_blank" rel="noreferrer">Space Invaders</a> : Jan 2023</h2>
+        <p>Built Space Invaders in Python using pygame.</p>
+        <ol>Learned: <br><br>
+            <li>Game logic</li>
+            <li>Classes, inheritance, polymorphism</li>
+            <li>Managing files depending on classes/subclasses</li>
+            <li>Collaborating and working using Git/GitHub</li>
+        </ol>
+    </section>
 </div>
 
 <style>
